bd797c75da0543681a3a646b2aa18416977a4451
Bohdan <boghdan.shimko@gmail.com>
2024-04-19 19:36:37 +0300

    add: search params

--- src/components/PostData.jsx
+++ src/components/PostData.jsx
@@ -1,19 +1,23 @@
 import { Link, useAsyncValue } from 'react-router-dom'
 
-const PostData = () => {
+const PostData = ({ startFrom, postQuery }) => {
   const data = useAsyncValue()
 
   return (
     <>
-      {data.map((v) => (
-        <Link
-          className='flex py-2 m-auto px-4 w-1/2 rounded-md bg-slate-700'
-          to={`/posts/${v.id}`}
-          key={v.id}
-        >
-          <span className='text-green-300 text-lg'>{v.title}</span>
-        </Link>
-      ))}
+      {data
+        .filter((v) => v.title.includes(postQuery) && v.id >= startFrom)
+        .map((v) => (
+          <Link
+            className='flex py-2 m-auto px-4 w-1/2 rounded-md bg-slate-700'
+            to={`/posts/${v.id}`}
+            key={v.id}
+          >
+            <span className='text-green-300 text-lg'>
+              {v.id}. {v.title}
+            </span>
+          </Link>
+        ))}
     </>
   )
 }
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,6 +1,7 @@
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData, useSearchParams } from 'react-router-dom'
 import LazyData from '../components/LazyData.jsx'
 import PostData from '../components/PostData.jsx'
+import PostFilter from '../components/PostFilter.jsx'
 import getData from '../helpers/getData.js'
 
 export const postsLoader = async () => {
@@ -11,12 +12,21 @@ export const postsLoader = async () => {
 
 const Posts = () => {
   const { data } = useLoaderData()
+  const [searchParams, setSearchParams] = useSearchParams()
+  const postQuery = searchParams.get('post') || ''
+  const latest = searchParams.has('latest')
+  const startFrom = latest ? 80 : 1
 
   return (
     <div className='flex flex-col gap-2'>
       <h1 className='text-white text-center text-2xl my-10'>Posts Page</h1>
+      <PostFilter
+        setSearch={setSearchParams}
+        latest={latest}
+        postQuery={postQuery}
+      />
       <LazyData posts={data}>
-        <PostData />
+        <PostData startFrom={startFrom} postQuery={postQuery} />
       </LazyData>
     </div>
   )
